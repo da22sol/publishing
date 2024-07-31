df618539ad1ea20848374eef6698bd3e091a36b1
[SW5] 김다솔 <gyeoul299@gmail.com>
2024-07-31 15:55:24 +0900

    feat.퍼블추가

--- src/pages/main/section/pub/publishing.style.tsx
+++ src/pages/main/section/pub/publishing.style.tsx
@@ -1,154 +1,154 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Pub = styled.div`
-  width: 100%;
-  background: #fff;
-  font-family: "Pretendard";
-  position: -webkit-sticky;
-  position: sticky;
+    width: 100%;
+    background: #fff;
+    font-family: 'Pretendard';
+    position: -webkit-sticky;
+    position: sticky;
 `;
 
 export const PubH2 = styled.h2`
-  padding: 80px 50px;
-  text-align: right;
-  font-size: 70px;
-  font-weight: 800;
-  color: #fff;
-  -webkit-text-stroke: 1px #519d5a;
-  @media (max-width: 430px) {
-    text-align: center;
+    padding: 80px 50px;
+    text-align: right;
+    font-size: 70px;
+    font-weight: 800;
+    color: #fff;
+    -webkit-text-stroke: 1px #519d5a;
+    @media (max-width: 430px) {
+        text-align: center;
 
-    font-size: 56px;
-  }
+        font-size: 56px;
+    }
 `;
 
 export const PubBox = styled.div`
-  height: 100vh;
-  width: 100%;
+    height: 100vh;
+    width: 100%;
 `;
 
 export const port = styled.div`
-  background-color: #519d5a;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-wrap: wrap;
-
-  @media (max-width: 1200px) {
-    flex-wrap: nowrap;
-  }
-  @media (max-width: 760px) {
+    background-color: #519d5a;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
     flex-wrap: wrap;
-  }
+
+    @media (max-width: 1200px) {
+        flex-wrap: nowrap;
+    }
+    @media (max-width: 760px) {
+        flex-wrap: wrap;
+    }
 `;
 
 export const PubImg = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  img {
-    width: 700px;
-    max-height: 650px;
-  }
-  @media (max-width: 1200px) {
     img {
-      width: 500px;
+        width: 700px;
+        max-height: 650px;
     }
-  }
-  
+    @media (max-width: 1200px) {
+        img {
+            width: 500px;
+        }
+    }
+
     @media (max-width: 768px) {
-    img {
-      width: 400px;
+        img {
+            width: 400px;
+        }
     }
-  }
-  @media (max-width: 430px) {
-    img {
-      width: 300px;
-      max-height: 250px;
+    @media (max-width: 430px) {
+        img {
+            width: 300px;
+            max-height: 250px;
+        }
     }
-  }
 `;
 export const PubTxt = styled.div`
-  overflow: visible;
-  text-align: center;
-  color: #f4f4f4;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    overflow: visible;
+    text-align: center;
+    color: #f4f4f4;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  p {
-    line-height: 2;
-    font-size: 20px;
-  }
-  @media (max-width: 1024px) {
-    padding: 5vw 0;
     p {
-      line-height: 1.4;
-      font-size: 16px;
+        line-height: 2;
+        font-size: 20px;
     }
-  }
-  @media (max-width: 430px) {
-    margin-top: 10px;
-    p {
-      font-size: 14px;
-      line-height: 1.2;
+    @media (max-width: 1024px) {
+        padding: 5vw 0;
+        p {
+            line-height: 1.4;
+            font-size: 16px;
+        }
     }
-  }
+    @media (max-width: 430px) {
+        margin-top: 10px;
+        p {
+            font-size: 14px;
+            line-height: 1.2;
+        }
+    }
 `;
 export const PubH4 = styled.h4`
-  line-height: 1;
-  font-size: 25px;
-  font-style: italic;
+    line-height: 1;
+    font-size: 25px;
+    font-style: italic;
 `;
 
 export const PubTH2 = styled.h2`
-  font-size: 45px;
-  padding: 18px 0;
-  @media (max-width: 768px) {
-    font-size: 30px;
-  }
+    font-size: 45px;
+    padding: 18px 0;
+    @media (max-width: 768px) {
+        font-size: 30px;
+    }
 `;
 
 export const SiteBtn = styled.div`
-  overflow: visible;
-  margin: 24px 0;
+    overflow: visible;
+    margin: 24px 0;
 
-  a {
-    padding: 10px;
-    border: 1px solid #fff;
-    border-radius: 15%;
-    text-align: center;
-    margin-right: 10px;
-    font-size: 12px;
-    color: #fff;
-  }
+    a {
+        padding: 10px;
+        border: 1px solid #fff;
+        border-radius: 15%;
+        text-align: center;
+        margin-right: 10px;
+        font-size: 12px;
+        color: #fff;
+    }
 
-  a:hover {
-    background-color: #f6edd9;
-    border: 1px solid #f6edd9;
-    color: #000;
-  }
+    a:hover {
+        background-color: #f6edd9;
+        border: 1px solid #f6edd9;
+        color: #000;
+    }
 `;
 
 export const deajeon = styled.div`
-  background-color: #e7f2e4;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-wrap: wrap;
-  @media (max-width: 1200px) {
-    flex-wrap: nowrap;
-  }
-  @media (max-width: 760px) {
+    background-color: #e7f2e4;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
     flex-wrap: wrap;
-  }
+    @media (max-width: 1200px) {
+        flex-wrap: nowrap;
+    }
+    @media (max-width: 760px) {
+        flex-wrap: wrap;
+    }
 `;
 
 export const PubTxtB = styled.div`
@@ -179,57 +179,92 @@ export const PubTxtB = styled.div`
 `;
 
 export const SiteBtn2 = styled.div`
-  overflow: visible;
-  margin: 24px 0;
-  a {
-    padding: 10px;
-    border: 1px solid #000000;
-    border-radius: 15%;
-    text-align: center;
-    margin-right: 10px;
-    font-size: 12px;
-    color: #000;
-  }
+    overflow: visible;
+    margin: 24px 0;
+    a {
+        padding: 10px;
+        border: 1px solid #000000;
+        border-radius: 15%;
+        text-align: center;
+        margin-right: 10px;
+        font-size: 12px;
+        color: #000;
+    }
 
-  a:hover {
-    background-color: #69b97c;
-    border: 1px solid #69b97c;
-    color: #fff;
-  }
+    a:hover {
+        background-color: #69b97c;
+        border: 1px solid #69b97c;
+        color: #fff;
+    }
 `;
 
 export const company = styled.div`
-  background-color: #ae3b4d;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-wrap: wrap;
-  @media (max-width: 1200px) {
-    flex-wrap: nowrap;
-  }
-  @media (max-width: 760px) {
+    background-color: #ae3b4d;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
     flex-wrap: wrap;
-  }
+    @media (max-width: 1200px) {
+        flex-wrap: nowrap;
+    }
+    @media (max-width: 760px) {
+        flex-wrap: wrap;
+    }
 `;
 export const SiteBtn3 = styled.div`
-  overflow: visible;
-  margin: 24px 0;
-  a {
-    padding: 10px;
-    border: 1px solid #fff;
-    border-radius: 15%;
-    text-align: center;
-    margin-right: 10px;
-    font-size: 12px;
-    color: #fff;
-  }
+    overflow: visible;
+    margin: 24px 0;
+    a {
+        padding: 10px;
+        border: 1px solid #fff;
+        border-radius: 15%;
+        text-align: center;
+        margin-right: 10px;
+        font-size: 12px;
+        color: #fff;
+    }
 
-  a:hover {
-    background-color: #a31f33;
-    border: 1px solid #a31f33;
-    color: #000;
-  }
+    a:hover {
+        background-color: #a31f33;
+        border: 1px solid #a31f33;
+        color: #000;
+    }
+`;
+export const amuse = styled.div`
+    background-color: #f8f4f3;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    flex-wrap: wrap;
+    @media (max-width: 1200px) {
+        flex-wrap: nowrap;
+    }
+    @media (max-width: 760px) {
+        flex-wrap: wrap;
+    }
+`;
+export const SiteBtn4 = styled.div`
+    overflow: visible;
+    margin: 24px 0;
+    a {
+        padding: 10px;
+        border: 1px solid #000;
+        border-radius: 15%;
+        text-align: center;
+        margin-right: 10px;
+        font-size: 12px;
+        color: #000;
+    }
+
+    a:hover {
+        background-color: #ff6cca;
+        border: 1px solid #ff6cca;
+        color: #000;
+    }
 `;
--- src/pages/main/section/pub/publishing.tsx
+++ src/pages/main/section/pub/publishing.tsx
@@ -1,104 +1,124 @@
-import React, { useRef, useEffect } from "react";
-import Port from "../../../..//assets/image/por.png";
-import Daejeon from "../../../../assets/image/dae.png";
-import Company from "../../../../assets/image/com.png";
-
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import * as S from "./publishing.style";
+import React, { useRef, useEffect } from 'react';
+import Port from '../../../..//assets/image/por.png';
+import Daejeon from '../../../../assets/image/dae.png';
+import Company from '../../../../assets/image/com.png';
+import Amuse from '../../../../assets/image/amuse.png';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import * as S from './publishing.style';
 
 const Publishing = () => {
-  const Slide1 = useRef(null);
-  const Slide2 = useRef(null);
-  const Slide3 = useRef(null);
+    const Slide1 = useRef(null);
+    const Slide2 = useRef(null);
+    const Slide3 = useRef(null);
+    const Slide4 = useRef(null);
 
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
 
-    const slides = [Slide1.current, Slide2.current, Slide3.current];
+        const slides = [Slide1.current, Slide2.current, Slide3.current, Slide4.current];
 
-    slides.forEach((slide, index) => {
-      gsap.from(slide, {
-        y: 100,
-        scrollTrigger: {
-          trigger: slide,
-          start: "top top",
-          end: "bottom top",
-          toggleActions: "play none none reverse",
-          scrub: true,
-          pin: true,
-          pinSpacing: false,
-        },
-      });
-    });
-  }, []);
+        slides.forEach((slide, index) => {
+            gsap.from(slide, {
+                y: 100,
+                scrollTrigger: {
+                    trigger: slide,
+                    start: 'top top',
+                    end: 'bottom top',
+                    toggleActions: 'play none none reverse',
+                    scrub: true,
+                    pin: true,
+                    pinSpacing: false,
+                },
+            });
+        });
+    }, []);
 
-  return (
-    <S.Pub>
-      <S.PubH2>Publishing</S.PubH2>
-      <S.PubBox>
-        <S.port ref={Slide1}>
-          <S.PubImg>
-            <img src={Port} alt="포트폴리오 이미지" />
-          </S.PubImg>
-          <S.PubTxt>
-            <S.PubH4>1/3</S.PubH4>
-            <S.PubTH2>개인 포트폴리오</S.PubTH2>
-            <p>리액트-타입스크립트 기반으로 만든 개인포트폴리오 사이트</p>
-            <S.SiteBtn>
-              <a href="https://github.com/da22sol/publishing">git에서보기</a>
-              <a href="https://frosted-brook-b69.notion.site/a750decbce704ec2821c5030ad4f6892">
-                코드리뷰
-              </a>
-            </S.SiteBtn>
-          </S.PubTxt>
-        </S.port>
-      </S.PubBox>
+    return (
+        <S.Pub>
+            <S.PubH2>Publishing</S.PubH2>
+            <S.PubBox>
+                <S.port ref={Slide1}>
+                    <S.PubImg>
+                        <img src={Port} alt="포트폴리오 이미지" />
+                    </S.PubImg>
+                    <S.PubTxt>
+                        <S.PubH4>1/4</S.PubH4>
+                        <S.PubTH2>개인 포트폴리오</S.PubTH2>
+                        <p>리액트-타입스크립트 기반으로 만든 개인포트폴리오 사이트</p>
+                        <S.SiteBtn>
+                            <a href="https://github.com/da22sol/publishing">git에서보기</a>
+                            <a href="https://frosted-brook-b69.notion.site/a750decbce704ec2821c5030ad4f6892">
+                                코드리뷰
+                            </a>
+                        </S.SiteBtn>
+                    </S.PubTxt>
+                </S.port>
+            </S.PubBox>
 
-      <S.PubBox>
-        <S.deajeon ref={Slide2}>
-          <S.PubImg>
-            <img src={Daejeon} alt="포트폴리오 이미지" />
-          </S.PubImg>
-          <S.PubTxtB>
-            <S.PubH4>2/3</S.PubH4>
-            <S.PubTH2>대전광역시 사회서비스원</S.PubTH2>
-            <p>공식사이트와 타지역사이트 디자인을 참고하며 만들었습니다.</p>
-            <p> 웹 접근성을 준수하여만들었고 반응형 웹사이트입니다</p>
-            <S.SiteBtn2>
-              <a href="https://da22sol.github.io/redaejeon/">site</a>
-              <a href="https://github.com/da22sol/redaejeon">git에서보기</a>
-              <a href="https://frosted-brook-b69.notion.site/f07895ae2d24419381a8db180250414f">
-                코드리뷰
-              </a>
-            </S.SiteBtn2>
-          </S.PubTxtB>
-        </S.deajeon>
-      </S.PubBox>
-
-      <S.PubBox>
-        <S.company ref={Slide3}>
-          <S.PubImg>
-            {" "}
-            <img src={Company} alt="포트폴리오 이미지" />
-          </S.PubImg>
-          <S.PubTxt>
-            <S.PubH4>3/3</S.PubH4>
-            <S.PubTH2>컴퍼니 회사 사이트</S.PubTH2>
-            <p>가상의 회사 사이트를 gdweb를 참고하여 만들었습니다.</p>
-            <p> fullPage.js 이루어진 반응형 웹사이트입니다</p>
-            <S.SiteBtn3>
-              <a href="https://da22sol.github.io/company">site</a>
-              <a href="https://github.com/da22sol/company">git에서보기</a>
-              <a href="https://frosted-brook-b69.notion.site/3b51d434d5b94d3c998bfd86c79490c2?pvs=74">
-                코드리뷰
-              </a>
-            </S.SiteBtn3>
-          </S.PubTxt>
-        </S.company>
-      </S.PubBox>
-    </S.Pub>
-  );
+            <S.PubBox>
+                <S.deajeon ref={Slide2}>
+                    <S.PubImg>
+                        <img src={Daejeon} alt="포트폴리오 이미지" />
+                    </S.PubImg>
+                    <S.PubTxtB>
+                        <S.PubH4>2/4</S.PubH4>
+                        <S.PubTH2>대전광역시 사회서비스원</S.PubTH2>
+                        <p>공식사이트와 타지역사이트 디자인을 참고하며 만들었습니다.</p>
+                        <p> 웹 접근성을 준수하여만들었고 반응형 웹사이트입니다</p>
+                        <S.SiteBtn2>
+                            <a href="https://da22sol.github.io/redaejeon/">site</a>
+                            <a href="https://github.com/da22sol/redaejeon">git에서보기</a>
+                            <a href="https://frosted-brook-b69.notion.site/f07895ae2d24419381a8db180250414f">
+                                코드리뷰
+                            </a>
+                        </S.SiteBtn2>
+                    </S.PubTxtB>
+                </S.deajeon>
+            </S.PubBox>
+            <S.PubBox>
+                <S.amuse ref={Slide4}>
+                    <S.PubImg>
+                        <img src={Amuse} alt="포트폴리오 이미지" />
+                    </S.PubImg>
+                    <S.PubTxtB>
+                        <S.PubH4>3/4</S.PubH4>
+                        <S.PubTH2>컴퍼니 회사 사이트</S.PubTH2>
+                        <p>가상의 회사 사이트를 gdweb를 참고하여 만들었습니다.</p>
+                        <p> fullPage.js 이루어진 반응형 웹사이트입니다</p>
+                        <S.SiteBtn4>
+                            <a href="https://da22sol.github.io/amuse">site</a>
+                            <a href="https://github.com/da22sol/amuse">git에서보기</a>
+                            <a href="https://frosted-brook-b69.notion.site/72dd5648876e402b8e895573b571fd2a">
+                                코드리뷰
+                            </a>
+                        </S.SiteBtn4>
+                    </S.PubTxtB>
+                </S.amuse>
+            </S.PubBox>
+            <S.PubBox>
+                <S.company ref={Slide3}>
+                    <S.PubImg>
+                        {' '}
+                        <img src={Company} alt="포트폴리오 이미지" />
+                    </S.PubImg>
+                    <S.PubTxt>
+                        <S.PubH4>4/4</S.PubH4>
+                        <S.PubTH2>컴퍼니 회사 사이트</S.PubTH2>
+                        <p>가상의 회사 사이트를 gdweb를 참고하여 만들었습니다.</p>
+                        <p> fullPage.js 이루어진 반응형 웹사이트입니다</p>
+                        <S.SiteBtn4>
+                            <a href="https://da22sol.github.io/company">site</a>
+                            <a href="https://github.com/da22sol/company">git에서보기</a>
+                            <a href="https://frosted-brook-b69.notion.site/3b51d434d5b94d3c998bfd86c79490c2?pvs=74">
+                                코드리뷰
+                            </a>
+                        </S.SiteBtn4>
+                    </S.PubTxt>
+                </S.company>
+            </S.PubBox>
+        </S.Pub>
+    );
 };
 
 export default Publishing;
