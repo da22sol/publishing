1236719f5b0f09d653cef24fa668385e8a39acd8
dasol <gyeoul299@gmail.com>
2024-08-30 01:29:43 +0900

    feat.기능수정

--- src/pages/main/section/skills/skills.tsx
+++ src/pages/main/section/skills/skills.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import * as S from './skills.style';
+
 const Skills = () => {
     const AboutScroll = useRef(null);
     const AboutUl = useRef(null);
